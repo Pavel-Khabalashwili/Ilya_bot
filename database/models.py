e013d36d696045de7e6427ba7de39d51c80aee9d
Pavel-Khabalashwili <139453616+Pavel-Khabalashwili@users.noreply.github.com>
2025-10-27 19:15:47 +0300

    add user model

--- database/models.py
+++ database/models.py
@@ -1,2 +1,37 @@
-import sqlalchemy
+from sqlalchemy import create_engine, Boolean, Column, DateTime, Integer, String, ForeignKey, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.decl_api import DeclarativeBase
+from sqlalchemy.orm import relationship, sessionmaker
 
+Base: DeclarativeBase = declarative_base()
+engine = create_engine('sqlite:///psychology_bot.db')
+
+class User(Base):
+    """Класс описывающий пользователя"""
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=True, index=True)
+    phone = Column(String(20), nullable=True)
+    email = Column(String(100), nullable=True)
+    created_at = Column(DateTime, server_default=func.now())
+
+    def __repr__(self):
+        """Возвращает строковое представление объекта."""
+
+        return (f"<User("
+                f"id={self.id}, "
+                f"name='{self.name}', "
+                f"last_name={self.last_name}, "
+                f"telegram_id={self.telegram_id})>")
+
+
+class PsychologicalRequest(Base):
+    """Класс описывающий запросы пользователя"""
+
+    id = Column(Integer, primary_key=True)
+    request_text = Column(String(2000), nullable=False)
+    status = Column(Boolean, default=False)
